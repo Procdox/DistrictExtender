65b5f502c6c1179668fb44e0c72d38b750b283df
Andrew <blackledgecreates@gmail.com>
2021-09-28 10:59:54 -0700

    cleanup dead code

--- DistrictExtender/Class1.cs
+++ DistrictExtender/Class1.cs
@@ -18,36 +18,6 @@ namespace DistrictExtender
         private static int Resource = 20;
         private static int Terrain = 10;
 
-        /*[HarmonyPrefix]
-        [HarmonyPatch(typeof(NavigationService), "FindPathUncachedLimitedRange")]
-        static bool FPULROverride(NavigationService __instance, bool __result, UnityEngine.Vector3 start, UnityEngine.Vector3 end, out float distance, List<UnityEngine.Vector3> pathCorners)
-        {
-            if (__instance.DestinationIsReachableUnlimitedRange(start, end, out distance, pathCorners))
-            {
-                __result = __instance._pathfindingService.FindPathUncached(start, end, 300f, out distance, pathCorners);
-            }
-            else
-            {
-                __result = false;
-            }
-            return false;
-        }
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(DistrictMap), "RecalculateRoadFlowField")]
-        static bool RRFFOverride(DistrictMap __instance, AccessFlowField roadFlowField, int districtCenterNodeId)
-        {
-            __instance._districtRoadFlowFieldGenerator.FillFlowFieldUpToDistance(__instance._roadNavMeshGraph, __instance._districtObstacleService, roadFlowField, 300f, districtCenterNodeId);
-            return false;
-        }
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(PathMeshDrawer), "DistanceToColor")]
-        static bool DTCOverride(PathMeshDrawer __instance, UnityEngine.Color __result, float distance)
-        {
-            float time = UnityEngine.Mathf.InverseLerp(0f, 300f, distance);
-            __result = __instance._distanceGradient.Evaluate(time);
-            return false;
-        }*/
-
         [HarmonyPrefix]
         [HarmonyPatch(typeof(NavigationDistance), "Load")]
         static bool DTCOverride(NavigationDistance __instance)
